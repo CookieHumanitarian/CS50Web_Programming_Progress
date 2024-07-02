0f99bfe44193a9ad71dcedc35b0eaacccb4eed5d
William Marcus <wmarcus2701@gmail.com>
2024-07-02 12:33:59 +0800

    Added 3 models and defined Listing class attributes

--- Project_2/auctions/models.py
+++ Project_2/auctions/models.py
@@ -4,3 +4,14 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField
+    startBid = models.IntegerField
+    image = models.ImageField
+class Bids(models.Model):
+    pass
+
+class Comments(models.Model):
+    pass
