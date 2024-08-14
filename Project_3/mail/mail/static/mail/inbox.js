db7000d951372197fefc43d69b89ccfea1ac5c2b
William Marcus <wmarcus2701@gmail.com>
2024-08-14 12:33:40 +0800

    Added send email function

--- Project_3/mail/mail/static/mail/inbox.js
+++ Project_3/mail/mail/static/mail/inbox.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
+  document.querySelector('#compose-form').addEventListener('submit', send_email)
 
   // By default, load the inbox
   load_mailbox('inbox');
@@ -20,6 +21,7 @@ function compose_email() {
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+
 }
 
 function load_mailbox(mailbox) {
@@ -31,3 +33,28 @@ function load_mailbox(mailbox) {
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 }
+
+function send_email(event) {
+  event.preventDefault();
+  
+  // Send email
+  fetch('/emails', {
+      method: 'POST',
+      body: JSON.stringify({
+        recipients: document.querySelector('#compose-recipients').value,
+        subject: document.querySelector('#compose-subject').value,
+        body: document.querySelector('#compose-body').value
+      })
+    })
+    .then(response => response.json())
+    .then(result => {
+        // Check for errors
+        if (result.error){
+          alert(JSON.stringify(result['error']))
+        }
+        else {
+          alert(JSON.stringify(result['message']))
+          load_mailbox('inbox')
+        }
+    });
+  }
